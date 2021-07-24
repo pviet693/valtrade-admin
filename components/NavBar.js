380b9c6fcf0ff52be895c254ab2139f37ec78c23
phamviet <viet.pv@ari.com.vn>
2021-07-24 14:12:42 +0700

    commit update logo

--- components/NavBar.js
+++ components/NavBar.js
@@ -32,8 +32,8 @@ const NavBar = () => {
     return (
         <>
             <nav className="navbar navbar-default navbar-fixed-top">
-                <div className="brand pt-0 pb-0 pl-0" style={{ paddingRight: "20px" }}>
-                    <Link href="/manage/product"><a><img src="/static/assets/img/logo.png" alt="Logo" className="img-responsive logo" width="260" height="75" /></a></Link>
+                <div className="brand pb-0 pl-4 pt-4">
+                    <Link href="/product"><a><img src="/static/assets/img/logo.jpg" alt="Logo" className="img-responsive logo" height="70" width="220" /></a></Link>
                 </div>
                 <div className="container-fluid">
                     <div className="navbar-btn">
@@ -47,26 +47,26 @@ const NavBar = () => {
                     </form>
                     <div id="navbar-menu">
                         <ul className="nav navbar-nav navbar-right">
-                            <li className="dropdown">
+                            {/* <li className="dropdown">
                                 <a href="#" className="dropdown-toggle icon-menu" data-toggle="dropdown">
                                     <i className="lnr lnr-alarm"></i>
                                     <span className="badge bg-danger">5</span>
                                 </a>
-                                <ul className="dropdown-menu notifications" style={{ height: '200px', background: '#fff' }}>
+                                <ul className="dropdown-menu notifications" style={{ height: '200px', background: '#fff' }}> */}
                                     {/* <li><a href="#" className="notification-item"><span className="dot bg-warning"></span>System space is almost full</a></li>
                                     <li><a href="#" className="notification-item"><span className="dot bg-danger"></span>You have 9 unfinished tasks</a></li>
                                     <li><a href="#" className="notification-item"><span className="dot bg-success"></span>Monthly report is available</a></li>
                                     <li><a href="#" className="notification-item"><span className="dot bg-warning"></span>Weekly meeting in 1 hour</a></li>
                                     <li><a href="#" className="notification-item"><span className="dot bg-success"></span>Your request has been approved</a></li>
                                     <li><a href="#" className="more">See all notifications</a></li> */}
-                                </ul>
-                            </li>
+                                {/* </ul>
+                            </li> */}
                             <li className="dropdown">
                                 <a href="#" className="dropdown-toggle" data-toggle="dropdown"><img src="/static/assets/img/user.png" className="img-circle" alt="Avatar" /> <span>{profile.name}</span> <i className="icon-submenu lnr lnr-chevron-down"></i></a>
                                 <ul className="dropdown-menu">
                                     <li><Link href="/profile"><a><i className="lnr lnr-user"></i> <span>Tài khoản của tôi</span></a></Link></li>
-                                    <li><a href="#"><i className="lnr lnr-envelope"></i> <span>Tin nhắn</span></a></li>
-                                    <li><a href="#"><i className="lnr lnr-cog"></i> <span>Cài đặt</span></a></li>
+                                    {/* <li><a href="#"><i className="lnr lnr-envelope"></i> <span>Tin nhắn</span></a></li>
+                                    <li><a href="#"><i className="lnr lnr-cog"></i> <span>Cài đặt</span></a></li> */}
                                     <li onClick={logout}><a href="#"><i className="lnr lnr-exit"></i> <span>Đăng xuất</span></a></li>
                                 </ul>
                             </li>
